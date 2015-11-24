de3d4fa747ffee61eb851c40867bfb47837c4548
Nicole Hall <nicolealexandrahall@gmail.com>
2015-11-24 10:49:34 -0700

    I was working on birds being created with a category but realized that the admin has to create the categories first. I'm switching to an admin branch to build out the admin capabilities and will revisit this branch and the skipped test after the admin is tested and built.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only:[:new, :create, :show]
+  resources :users, only:[:new, :create, :show] do
+    resources :birds
+  end
+
+  namespace :admin do
+    resources :species
+  end
 
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
